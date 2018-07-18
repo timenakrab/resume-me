4b424ad8f666f48f32a868670a6735b1649e0742
Watchara Hongkim <timenakrab@gmail.com>
2018-07-19 00:17:49 +0700

    อัพเดท ข้อมูล about me

--- src/components/IndexPage/IndexPage.jsx
+++ src/components/IndexPage/IndexPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Parallax, Row, Col, Card, CardTitle, Icon, } from 'react-materialize'
+import { Parallax, Row, Col, Button } from 'react-materialize'
 import {Link} from 'react-router-dom'
 import "./IndexPage.css"
 
@@ -16,8 +16,18 @@ export default class IndexPage extends Component {
                 <Parallax imageSrc="./assets/bg-homepage.jpg"/>
                 <div className="section white">
                     <div className="row container">
-                    <h2 className="header">Parallax</h2>
-                    <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                        <h2 className="header center-align text-redpink">About Me</h2>
+                        <Row>
+                            <Col s={6} m={3} l={3}>
+                                <img src="./assets/coffee.png" style={{width:"100%"}}/>
+                            </Col>
+                            <Col s={6} m={9} l={9}>
+                                <p className="grey-text text-darken-3 lighten-3 font-25">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                                <p className="grey-text text-darken-3 lighten-3 font-25 mt-10">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                                <Button className="btn-contact-now z-depth-0">Contact Now</Button>
+                                <Button className="btn-portfolio-now z-depth-0">Portfolio</Button>
+                            </Col>
+                        </Row>
                     </div>
                 </div>
             </div>
